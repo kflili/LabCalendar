97b4f02ecbcd449ca8ef83fe603a5cd8be224d9b
LiLi <kflili@gmail.com>
2017-07-21 16:02:14 -0400

    Install Entity Framework, Set up Database Context

---
+++ LabCalendar/DAL/LabContext.cs
@@ -0,0 +1,18 @@
+﻿using LabCalendar.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LabCalendar.DAL
+{
+    public class LabContext : DbContext
+    {
+        public LabContext() : base("LabContext")
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+    }
+}
