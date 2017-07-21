77451c51c7f015f6734e55e8b88d318326a83acf
LiLi <kflili@gmail.com>
2017-07-21 15:49:17 -0400

    Create Event Model

---
+++ LabCalendar/Models/Event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LabCalendar.Models
+{
+    public class Event
+    {
+        public int EventID { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Start { get; set; }
+
+        public DateTime? End { get; set; }
+
+        public string ThemeColor { get; set; }
+
+        public byte IsFullDay { get; set; }
+    }
+}
