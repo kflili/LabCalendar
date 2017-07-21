eb47c3e4c5aa382a5077af1a9653a8b9e755eab2
LiLi <kflili@gmail.com>
2017-07-21 16:34:39 -0400

    Enable connection resiliency

---
+++ LabCalendar/DAL/LabConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.SqlServer;
+using System.Linq;
+using System.Web;
+
+namespace LabCalendar.DAL
+{
+    public class LabConfiguration : DbConfiguration
+    {
+        public LabConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new SqlAzureExecutionStrategy());
+        }
+    }
+}
