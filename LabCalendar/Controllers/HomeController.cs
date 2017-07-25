833e945131401aacba2fb246aa553083151f186a
LiLi <kflili@gmail.com>
2017-07-24 23:00:10 -0400

    add delete, edit or update action

--- LabCalendar/Controllers/HomeController.cs
+++ LabCalendar/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using LabCalendar.DAL;
+using LabCalendar.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +23,55 @@ namespace LabCalendar.Controllers
                 var events = db.Events.ToList();
                 return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
             }
+        }
+
+        [HttpPost]
+        public JsonResult SaveEvents(Event e)
+        {
+            var status = false;
+            using (LabContext db = new LabContext())
+            {
+                if (e.EventID > 0)
+                {
+                    // Update the event
+                    var v = db.Events.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Description = e.Description;
+                        v.ThemeColor = e.ThemeColor;
+                        v.IsFullDay = e.IsFullDay;
+                    }
+                }
+                else
+                {
+                    db.Events.Add(e);
+                }
+                db.SaveChanges();
+                status = true;
+            }
+
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (LabContext db = new LabContext())
+            {
+                var v = db.Events.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    db.Events.Remove(v);
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+
+            return new JsonResult { Data = new { status = status } };
         }
     }
 }
